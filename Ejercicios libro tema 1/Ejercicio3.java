d9e4a1fc95bcac295a6e8b9d75768f39bdb7a220
mariasnchez <91953276+mariasnchez@users.noreply.github.com>
2021-10-19 12:15:49 +0200

    Add files via upload

---
+++ Ejercicios libro tema 1/Ejercicio3.java
@@ -0,0 +1,15 @@
+public class Ejercicio3{
+public static void main (String [] args) {
+  
+System.out.printf("%-10s %-10s\n","nature","naturaleza");
+System.out.printf("%-10s %s\n","potato","patata");
+System.out.printf("%-10s %-10s\n","t-shirt","camiseta");
+System.out.printf("%-10s %s\n","eye","ojo");
+System.out.printf("%-10s %-10s\n","winter","invierno");
+System.out.printf("%-10s %s\n","water","agua");
+System.out.printf("%-10s %-10s\n","pencil","lápiz");
+System.out.printf("%-10s %s\n","shoes","zapatos");
+System.out.printf("%-10s %-10s\n","sky","cielo");
+System.out.printf("%-10s %s\n","paper","papel");
+}
+}
---
+++ Ejercicios libro tema 1/Ejercicio8.java
@@ -0,0 +1,10 @@
+public class Ejercicio8{
+public static void main (String [] args) {
+System.out.println("* * * *");
+System.out.printf("%2s%4s","*","*");
+System.out.println();
+System.out.printf("%3s%2s","*","*");
+System.out.println();
+System.out.printf("%4s","*");
+}
+}
